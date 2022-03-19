d3d3d9657f6f7190f3c8ece205de5dc318aceb93
LetsPlayKeks <letsplaykeks07@gmail.com>
2022-03-19 14:11:17 +0100

    Updates:
    updated cookie_dough -> is now food item /w effects
    updateddev_letsplaykeks -> is now food item

--- src/main/java/net/painandsuffering/pas/PaSFood.java
+++ src/main/java/net/painandsuffering/pas/PaSFood.java
@@ -1,6 +1,9 @@
 package net.painandsuffering.pas;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -10,6 +13,13 @@ public class PaSFood {
 	public static final Item COOKIE_DOUGH = registerItem("cookie_dough", new Item(new FabricItemSettings()
 			.maxCount(16)
 			.group(PaSMainMod.PAS_FOOD_GROUP)
+			.food(new FoodComponent.Builder()
+					.alwaysEdible()
+					.hunger(2)
+					.saturationModifier(1f)
+					.statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*4, 3), .6f)
+					.statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 20*10, 1), 1f)
+					.build())
 			));
 	
 	
--- src/main/java/net/painandsuffering/pas/PaSItemsMisc.java
+++ src/main/java/net/painandsuffering/pas/PaSItemsMisc.java
@@ -1,6 +1,9 @@
 package net.painandsuffering.pas;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -10,6 +13,11 @@ public class PaSItemsMisc {
 	public static final Item DEV_LETSPLAYKEKS = registerItem("dev_letsplaykeks", new Item(new FabricItemSettings()
 			.maxCount(1)
 			.group(PaSMainMod.PAS_MISC_GROUP)
+			.food(new FoodComponent.Builder()
+					.alwaysEdible()
+					.snack()
+					.statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1, 2), 1f)
+					.build())
 			));
 	public static final Item DEV_UWUIZLOST = registerItem("dev_uwuizlost", new Item(new FabricItemSettings()
 			.maxCount(1)
