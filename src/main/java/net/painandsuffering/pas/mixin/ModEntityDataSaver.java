849fbd4ab5a1c4f4d0a5e0c49d1916e668bb2804
LetsPlayKeks <letsplaykeks07@gmail.com>
2022-03-19 14:50:40 +0100

    added/injected readNbt and writeNbt methods in ModEntityDataSaver

--- src/main/java/net/painandsuffering/pas/mixin/ModEntityDataSaver.java
+++ src/main/java/net/painandsuffering/pas/mixin/ModEntityDataSaver.java
@@ -1,6 +1,10 @@
 package net.painandsuffering.pas.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NbtCompound;
@@ -10,6 +14,7 @@ import net.painandsuffering.util.IEntityDataSaver;
 public abstract class ModEntityDataSaver implements IEntityDataSaver{
 	private NbtCompound persistentData;
 	
+	
 	@Override
 	public NbtCompound getPersistentData() {
 		if(this.persistentData == null) {
@@ -17,5 +22,19 @@ public abstract class ModEntityDataSaver implements IEntityDataSaver{
 		}
 		return persistentData;
 	}
+	
+	@Inject(method = "writeNbt", at = @At("HEAD"))
+    protected void injectWriteMethod(NbtCompound nbt, CallbackInfoReturnable info) {
+        if (persistentData != null) {
+            nbt.put("PaSData", persistentData);
+        }
+    }
+
+    @Inject(method = "readNbt", at = @At("HEAD"))
+    protected void injectReadMethod(NbtCompound nbt, CallbackInfo info) {
+        if (nbt.contains("PaSData", 10)) {
+            persistentData = nbt.getCompound("PaSData");
+        }
+    }
 	
 }
